abdfda9619f5072c784f17c7c864492386334584
lenrich <lenrich1403@gmail.com>
2023-11-23 13:24:31 +0800

    add Function Practice

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-// Variables
-// import "./src/001variables/001-const-let-var";
+// // Variables
+// import "./src/001-variables/001-const-let-var";
 
-// Operators
-// import "./src/002operators/001-Type-Conversion-and-Coercion";
-// import "./src/002operators/002-Truthy-and-Falsy-Values";
-// import "./src/002operators/003-Equality-Operators-==vs===";
-// import "./src/002operators/004-Logical-Operators";
-// import "./src/002operators/005-The-switch-Statement";
-import "./src/002operators/006-The-Conditional-(Ternary)-Operator";
+// // Operators
+// import "./src/002-operators/001-Type-Conversion-and-Coercion";
+// import "./src/002-operators/002-Truthy-and-Falsy-Values";
+// import "./src/002-operators/003-Equality-Operators-==vs===";
+// import "./src/002-operators/004-Logical-Operators";
+// import "./src/002-operators/005-The-switch-Statement";
+// import "./src/002-operators/006-The-Conditional-(Ternary)-Operator";
+
+// Functions
+// import "./src/003-functions/001-Activating-Strict-Mode";
+// import "./src/003-functions/002-Function-Example";
+// import "./src/003-functions/003-Function-Declarations-vs-Expressions";
+// import "./src/003-functions/004-Arrow-Functions";
+import "./src/003-functions/005-Functions-Calling-Other-Functions";
