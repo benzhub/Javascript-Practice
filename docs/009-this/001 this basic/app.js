0639e44a0e33015162198e7b474405e13f9b6122
lenrich <lenrich1403@gmail.com>
2023-11-26 16:14:34 +0800

    add this practice

---
+++ docs/009-this/001 this basic/app.js
@@ -0,0 +1,38 @@
+const jack = {
+  name: "jack",
+  job: "firefighter",
+  birthYear: 1999,
+  age() {
+    console.log(this);
+    return 2023 - this.birthYear;
+  },
+};
+
+console.log(jack.age());
+// {name: 'jack', job: 'firefighter', birthYear: 1999, age: ƒ}
+// 24
+
+const mike = {
+  birthYear: 2001,
+};
+
+mike.age = jack.age;
+console.log(mike.age());
+// 我們可以發現this是動態的
+// 誰調用，this就會指向誰(但只能指向Object)
+// {birthYear: 2001, age: ƒ}
+// 22
+
+const x = jack.age;
+console.log(x()); // this不能指向function
+// Window {window: Window, self: Window, document: document, name: '', location: Location, …}
+// NaN
+
+const betty = (height, weight) => {
+  let BMI = weight / height ** 2;
+  console.log(BMI);
+  console.log(this); // 箭頭函數沒有自己的this，但它會顯示parent scope或是全域scope
+};
+betty(1.65, 55);
+// 20.202020202020204
+// Window {window: Window, self: Window, document: document, name: '', location: Location, …}
---
+++ docs/009-this/002 this in arrow function/app.js
@@ -0,0 +1,44 @@
+const betty = {
+  name: "Betty",
+  gender: "F",
+  birthYear: 1999,
+  height: 1.65,
+  weight: 55,
+  healthStatus() {
+    console.log(this);
+
+    // old school solution(將this 放到一個變數中傳遞下去)
+    const that = this;
+    function calcBodyFatPercentage() {
+      // console.log(that);
+      const bmiRsult = that.weight / that.height ** 2;
+      const age = new Date().getFullYear() - that.birthYear;
+      const genderRate = that.gender === "F" ? 0 : 10.8;
+      // console.log(`BMI: ${Math.round(bmiRsult*100)/100}`);
+      const bodyFatPercentResult =
+        1.2 * bmiRsult + (0.23 * age - 5.4) - genderRate;
+      // console.log(`Body Fat Percentage: ${Math.round(bodyFatPercentResult*100)/100}`)
+      return {
+        bmi: Math.round(bmiRsult * 100) / 100,
+        bodyFatPercent: Math.round(bodyFatPercentResult * 100) / 100,
+      };
+    }
+
+    // // Solution 2(使用arrow function的this去得到 parent scope 或 global scope)
+    // const calcBodyFatPercentage = () => {
+    //     console.log(this);
+    //     const bmiRsult = this.weight / this.height ** 2;
+    //     const age = new Date().getFullYear() - this.birthYear;
+    //     const genderRate = this.gender === "F" ? 0 : 10.8;
+    //     // console.log(`BMI: ${Math.round(bmiRsult*100)/100}`);
+    //     const bodyFatPercentResult = (1.2 * bmiRsult) + (0.23 * age - 5.4) - genderRate;
+    //     // console.log(`Body Fat Percentage: ${Math.round(bodyFatPercentResult*100)/100}`)
+    //     return { bmi: Math.round(bmiRsult*100)/100, bodyFatPercent: Math.round(bodyFatPercentResult*100)/100 }
+    // }
+    return calcBodyFatPercentage();
+  },
+};
+
+const { bmi, bodyFatPercent } = betty.healthStatus();
+console.log(bmi); // 20.2
+console.log(bodyFatPercent); // 24.36
